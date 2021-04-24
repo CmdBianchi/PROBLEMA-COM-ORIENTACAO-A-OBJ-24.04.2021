def2b426ede37ca1ceee6bbd519076e0ad051c76
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-04-24 15:02:20 -0300

    Adicionar arquivos de projeto.

---
+++ PROBLEMA COM ORIENTACAO A OBJ 24.04.2021/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace Problema_sem_orientação_a_objetos_24._04._2021{
+   class Program{
+        //-----> START / MAIN 
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        static void Main(string[] args){
+            Msg();
+            Triangulo x, y;
+            x = new Triangulo();
+            y = new Triangulo();
+            Console.WriteLine("Digite os valores do TRIANGULO X | 1º valor lado A, 2º valor lado B, 3º valor lado C");
+            x.A = double.Parse(Console.ReadLine()); x.B = double.Parse(Console.ReadLine()); x.C = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite os valores do TRIANGULO Y | 1º valor lado A, 2º valor lado B, 3º valor lado C");
+            y.A = double.Parse(Console.ReadLine()); y.B = double.Parse(Console.ReadLine()); y.C = double.Parse(Console.ReadLine());         
+            Console.WriteLine("A maior area é: " + EquationTriangleA(x.A, x.B, x.C, y.A, y.B, y.C).ToString("F2"));  
+        }
+        /// -------> FUNCTIONS
+        //////////////////////////////////////////////////////////////////////////////////////////// 
+        static void Msg(){
+            Console.WriteLine("Calcular a área de dois triângulos a partir das medidas de seus lados a, b e c");
+            Console.WriteLine("e dizer qual dos dois triângulospossui a maior área.");
+            Console.WriteLine("");
+        }
+        //-----------------------------------------------------------------------------------------//
+        static double EquationTriangleA(double xa, double xb, double xc, double ya, double yb, double yc){
+            double px = (xa + xb + xc) / 2;
+            double areax = Math.Sqrt(px * (px - xa) * (px - xb) * (px - xc));
+            double py = (ya + yb + yc) / 2;
+            double areay = Math.Sqrt(py * (py - ya) * (py - yb) * (py - yc));
+            Console.Write("AREA TRIANGULO X: "+areax);
+            Console.WriteLine("");
+            Console.Write("AREA TRIANGULO y: " +areay);
+            Console.WriteLine("");
+            if ( areax > areay) {
+                return areax;
+            }
+            else {
+                return areay;
+            }
+        }
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        //-----> END
+   }
+}
+
---
+++ PROBLEMA COM ORIENTACAO A OBJ 24.04.2021/Triangulo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Problema_sem_orientação_a_objetos_24._04._2021
+{
+    class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+    }
+}
